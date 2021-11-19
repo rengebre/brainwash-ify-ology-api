e427062333b5a55815a1e3bea9a41e0b3843e3d1
Sophie <sophiejoliver19+1@gmail.com>
2021-11-19 21:29:50 +0000

    testing post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
   def create
     puts "we pinged"
+    puts "here + #{params}"
+    @post = Post.new(post_params)
+
+    @post.save
+
+    if @post 
+      payload = {
+        success: 'much'
+      }
+      render :json => payload, :status => 200
+    else 
+      render :json => {error: "you baaaad"}, :status => 400
+    end
   end
 
   private
-
   def post_params
-
+    params.require(:post).permit(:title, :description)
   end
 end
