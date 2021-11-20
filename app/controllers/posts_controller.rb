f27b493ed7604b4a59909e10e01d206027a6260c
Sophie <sophiejoliver19+1@gmail.com>
2021-11-20 19:56:56 +0000

    add destroy method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    render :json => {message: "post successfully dEsTroYed"}, :status => 202
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :description)
