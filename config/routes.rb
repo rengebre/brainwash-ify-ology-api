3396cf4d41c7834be282b50ee8dc9fa14b5f46b7
Katie <katie.herda2@gmail.com>
2021-11-23 19:54:27 +0000

    added a create and destroy route for comments.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   resources :interests, only: [:index]
+  resources :comments, only: [:create, :destroy]
 end
