a3d0058fcc44328af98976cc0b87afbbc1312741
Katie <katie.herda2@gmail.com>
2021-11-23 21:16:45 +0000

    completed comment create method. Successfully saves a new comment for a given post id.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,27 @@
 class CommentsController < ApplicationController
 
   def create
+  
+    comment_parameters = {
+      :content => comment_params["content"],
+      :user_id => comment_params["user_id"],
+      :post_id => comment_params["post_id"]
+    }
+    
+    @comment = Comment.new(comment_parameters)
+    @comment.save!
+
   end
 
   def destroy
+
+  end
+  
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
   end
   
 end
