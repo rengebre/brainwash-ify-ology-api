6f241bdf2ee9df8f94374cb9b574a2678f4dfaa8
Sophie <sophiejoliver19+1@gmail.com>
2021-11-23 18:59:20 +0000

    update post index to display posts in descending order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(updated_at: :desc)
     @users = @posts.map { |post| post.user}
     # return_post = {}
 
