3261b85b9ea546ae02b6ea97d99b7abcc34fbe87
Katie <katie.herda2@gmail.com>
2021-11-24 04:50:35 +0000

    completed create method to save a new like to the likes table.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,24 @@
 class LikesController < ApplicationController
+  
   def create
+
+    like_parameters = {
+      :user_id => like_params["user_id"],
+      :post_id => like_params["post_id"]
+    }
+
+    @like = Like.new(like_parameters)
+    @like.save!
+
+    render :json => @like
   end
 
   def destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
   end
 end
