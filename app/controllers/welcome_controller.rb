8dc1d200bf998211c9acf91987cc2df318aab207
Sophie <sophiejoliver19+1@gmail.com>
2021-11-28 02:42:38 +0000

    first Heroku deploy

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ActionController::Base
+  def index
+    render :index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  
   resources :posts
   resources :interests, only: [:index]
   resources :follows, only: [:create, :destroy]
