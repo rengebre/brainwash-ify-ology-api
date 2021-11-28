8c1dbac32539b69c4f94f44cdbb7c6b33bce65ec
Sophie <sophiejoliver19+1@gmail.com>
2021-11-28 04:02:52 +0000

    add cloudinary files and update base URL

---
+++ config/initializers/cloudinary_example.rb
@@ -0,0 +1,7 @@
+# Cloudinary.config do |config|    
+#   config.cloud_name = "cloud_name"   
+#   config.api_key = "api_key"   
+#   config.api_secret = "api_secret"   
+#   config.secure = true    
+#   config.cdn_subdomain = true  
+# end
